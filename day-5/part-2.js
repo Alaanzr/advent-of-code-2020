867750c608d2134eec9f0c46e8c411b00b96075c
Alaan Rahim <alaan.rahim@kaluza.com>
2020-12-05 13:18:42 +0000

    Add day 5 part 2 solution

--- day-5/part-2.js
+++ day-5/part-2.js
@@ -3,7 +3,6 @@ const { read } = require('../utils')
 ;(async () => {
   const data = await read('./data.txt')
 
-
   const findHelper = (blocks, range, lowerHalfKey) => {
     const start = range[0]
     const end = range[1]
@@ -26,7 +25,9 @@ const { read } = require('../utils')
 
   const findRow = (rows) => findHelper(rows, [0, 127], 'F')
   const findColumn = (columns) => findHelper(columns, [0, 7], 'L')
+  
   let maxSeatID = 0
+  const seatIDs = []
 
   for (let i = 0; i < data.length; i++) {
     const ticket = data[i]
@@ -35,8 +36,17 @@ const { read } = require('../utils')
     const row = findRow(rowPartition)
     const column = findColumn(columnPartition)
     const seatID = (row * 8) + column
+    seatIDs.push(seatID)
     maxSeatID = Math.max(maxSeatID, seatID)
   }
 
-  console.log('max seat id: ', maxSeatID)
+  let seat
+  for (let i = 0; i < maxSeatID; i++) {
+    const isSeatOccupied = seatIDs.find(seatID => seatID === i)
+    if (!isSeatOccupied) {
+      seat = i
+    }
+  }
+
+  console.log(seat)
 })()
