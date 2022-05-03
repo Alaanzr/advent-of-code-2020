32dc66dc8088f8df26a5bdef6380c0fc318bee6a
Alaan Rahim <alaanzr@gmail.com>
2022-05-03 14:40:19 +0100

    merge freeze test

--- utils.js
+++ utils.js
@@ -1,13 +1,14 @@
-const { readFile } = require('fs');
-const { promisify } = require('util');
+const { readFile } = require("fs");
+const { promisify } = require("util");
 
 const readFileAsync = promisify(readFile);
 
-const read = async (path, delimiter = '\n') => {
-  const content = await readFileAsync(path, 'utf-8')
-  return content.split(delimiter)
+const read = async (path, delimiter = "\n") => {
+  console.log("merge-freeze");
+  const content = await readFileAsync(path, "utf-8");
+  return content.split(delimiter);
 };
 
 module.exports = {
-  read
-}
+  read,
+};
